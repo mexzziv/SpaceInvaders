c4d1ecd939e2c47e7e14aaf6bb9098710b776351
ivanmarquez <ivan_me_2@hotmail.com>
2023-06-09 14:16:41 -0600

    feat: :construction: Add alients to game
    Create function to draw alines inside the game

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame, sys
 from player import Player
 import obstacle
+from alien import Alien
 
 class Game:
     def __init__(self):
@@ -16,6 +17,10 @@ class Game:
         self.obstacle_x_positions = [num * (screen_width / self.obstacle_amount) for num in range(self.obstacle_amount)]
         self.create_multiple_obstacles(*self.obstacle_x_positions, x_start = screen_width / 15, y_start = 480)
 
+        # Alien setup
+        self.aliens = pygame.sprite.Group()
+        self.alien_setup(rows = 6, cols = 8)
+
     def create_obstacle(self, x_start, y_start, offset_x):
         for row_index, row in enumerate(self.shape):
             for col_index, col in enumerate(row):
@@ -28,6 +33,14 @@ class Game:
     def create_multiple_obstacles(self, *offset, x_start, y_start):
         for offset_x in offset:
             self.create_obstacle(x_start, y_start, offset_x)
+    
+    def alien_setup(self, rows, cols, x_distance = 60, y_distance = 48, x_offset = 70, y_offset = 25):
+        for row_index, row in enumerate(range(rows)):
+            for col_index, col in enumerate(range(cols)):
+                x = col_index * x_distance + x_offset
+                y = row_index * y_distance + y_offset
+                alien_sprite = Alien('red', x ,y)
+                self.aliens.add(alien_sprite)
 
     def run(self):
         self.player.update()
@@ -36,6 +49,7 @@ class Game:
         self.player.draw(screen)
 
         self.blocks.draw(screen)
+        self.aliens.draw(screen)
         # update all sprites groups
         # draw all sprite groups
 
